54e9358205e8c61f2475681006db4e88a2392842
tkoomzaaskz <tkoomzaaskz@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-24 16:52:40 -0600

    [td][video] corrected admin video display
    git-svn-id: http://svn.symfony-project.com/plugins/tdVideoPlugin/trunk@27147 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/td_video/templates/_file_show.php
+++ modules/td_video/templates/_file_show.php
@@ -1,6 +1,6 @@
 <?php use_helper('Video') ?>
 
-<a href="<?php echo video_path('flv'.'/'.$td_video['file']) ?>" style="display:block;width:520px;height:330px" id="player"></a>
+<a href="<?php echo '/uploads/td/video/'.$td_video['file'] ?>" style="display:block;width:520px;height:330px" id="player"></a>
 
 <script>
   flowplayer("player", "<?php echo video_path('swf/flowplayer-3.1.5.swf') ?>");
