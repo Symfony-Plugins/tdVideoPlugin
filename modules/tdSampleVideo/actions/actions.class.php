b25431741999ad50af3e62db46dad85a67c3a376
tkoomzaaskz <tkoomzaaskz@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-02-23 06:39:51 -0600

    [td][video] forward404 if no video added
    git-svn-id: http://svn.symfony-project.com/plugins/tdVideoPlugin/trunk@28209 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/tdSampleVideo/actions/actions.class.php
+++ modules/tdSampleVideo/actions/actions.class.php
@@ -12,23 +12,28 @@ class tdSampleVideoActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
+    $this->videos = Doctrine::getTable('tdVideo')
+      ->getActiveVideosQuery()
+      ->fetchArray();
+
+    $this->forward404Unless(count($this->videos) > 0);
+
     // ading default td_video layout
     $this->getResponse()->addStylesheet('/tdVideoPlugin/css/td_video');
-
-    $this->videos = Doctrine::getTable('tdVideo')->getActiveVideosQuery()->fetchArray();
   }
 
   public function executeShow(sfWebRequest $request)
   {
-    // ading default td_video layout
-    $this->getResponse()->addStylesheet('/tdVideoPlugin/css/td_video');
-
-    $results = Doctrine::getTable('tdVideo')->getActiveVideoByFileQuery($request->getParameter('file'))->fetchArray();
-    $this->video = $results[0];
+    $this->forward404Unless($collection = Doctrine::getTable('tdVideo')
+      ->getActiveVideoByFileQuery($request->getParameter('file'))
+      ->fetchArray());
+    
+    $this->video = $collection[0];
 
     // adding js flash embedding script
     $this->getResponse()->addJavascript('/tdVideoPlugin/js/flowplayer-3.1.4.min.js');
-
+    // ading default td_video layout
+    $this->getResponse()->addStylesheet('/tdVideoPlugin/css/td_video');
     // adding default flowplayer stylesheet
     $this->getResponse()->addStylesheet('/tdVideoPlugin/css/flowplayer');
   }
